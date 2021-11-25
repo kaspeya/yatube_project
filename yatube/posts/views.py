c997c6d9095d45dc236422efb30e0234cbbaf84d
Елизавета Шалаева <kaspeya@yandex.com>
2021-11-25 23:57:50 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Главная страница
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+# Страница с постами, отфильтрованные по группам.
+def group_posts(request,pk):
+    return HttpResponse(f'Посты, отфильтрованные по группам {pk}')
